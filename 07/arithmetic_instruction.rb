a3c3c02f3b86129af1546761be30ee6a8ce4e689
Dan Drust <dan.drust@protrainings.com>
2018-03-16 21:52:40 -0400

    Don’t overwrite stack pointer, write to address held in stack pointer

--- 07/arithmetic_instruction.rb
+++ 07/arithmetic_instruction.rb
@@ -10,12 +10,13 @@ class ArithmeticInstruction < Instruction
     when :add
       Array.new
         .push("@SP")   # Load address of stack pointer
+        .push("A=M")   # Load value of SP into A register
         .push("A=A-1") # subtract one from stack pointer (1st val)
         .push("D=M")   # load 1st val into data register
         .push("A=A-1") # subtract one from stack pointer (2nd val)
         .push("M=D+M") # write result of M+D to stack pointer
         .push("D=A+1") # increment stack pointer
-        .push("@0")
+        .push("@SP")
         .push("M=D")
     when :sub
       
--- 07/push_instruction.rb
+++ 07/push_instruction.rb
@@ -11,9 +11,10 @@ class PushInstruction < Instruction
         .push("@#{index.to_i}") # Load the constant into the A register
         .push("D=A")            # "Compute" by passing A register value to D register
         .push("@SP")            # Load SP address to A register
+        .push("A=M")            # Load value of SP into A register
         .push("M=D")            # Write M[A] = D
         .push("D=A+1")          # Increment the stack pointer
-        .push("@0")
+        .push("@SP")
         .push("M=D")
       # Call instruction.new('whatever').to_assembly.each {|instruction| file.puts instruction }
     end
