315bb8ea087e174bd662e80b38bfa0d9c1d74492
Dan Drust <dan.drust@protrainings.com>
2018-03-20 21:38:15 -0400

    Make instruction a singleton class

--- 07/instruction.rb
+++ 07/instruction.rb
@@ -1,30 +1,30 @@
 class Instruction
+  class << self
+    attr_reader :string, :operation, :segment, :index
 
-  attr_reader :string, :operation, :segment, :index
+    def parse string
+      resolve_instruction string.chomp.strip
+    end
+    
+    private
 
-  def self.parse string
-    resolve_instruction string.chomp.strip
-  end
-  
-  private
-
-  def self.resolve_instruction instruction
-    regex = /^(\w*)\s*(\w*)\s*(\w*)\s*/
-    parsed_instruction = instruction.match regex
-    operation = parsed_instruction[1].to_sym
-    segment   = parsed_instruction[2].to_sym
-    index     = parsed_instruction[3]
-    case operation
-    when :push
-      PushInstruction.new operation, segment, index
-    else
-      ArithmeticInstruction.new operation
+    def resolve_instruction instruction
+      if instruction.empty? or instruction.nil? or instruction.match /^\/\//
+        return Comment.new instruction
+      end
+      regex = /^(\w*)\s*(\w*)\s*(\w*)\s*/
+      parsed_instruction = instruction.match regex
+      operation = parsed_instruction[1].to_sym
+      segment   = parsed_instruction[2].to_sym
+      index     = parsed_instruction[3]
+      case operation
+      when :push
+        PushInstruction.new operation, segment, index
+      else
+        ArithmeticInstruction.new operation
+      end
     end
   end
-  
-  
-
-
 end
 
 require './push_instruction'
