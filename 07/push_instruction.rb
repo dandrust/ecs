fe91be60c3bee4d25fa02afee2c052ba86aa926c
Dan Drust <dan.drust@protrainings.com>
2018-03-20 21:37:49 -0400

    refactor push instruction

--- 07/push_instruction.rb
+++ 07/push_instruction.rb
@@ -7,15 +7,14 @@ class PushInstruction < Instruction
   def to_assembly
     case @segment
     when :constant
-      Array.new
-        .push("@#{index.to_i}") # Load the constant into the A register
-        .push("D=A")            # "Compute" by passing A register value to D register
-        .push("@SP")            # Load SP address to A register
-        .push("A=M")            # Load value of SP into A register
-        .push("M=D")            # Write M[A] = D
-        .push("D=A+1")          # Increment the stack pointer
-        .push("@SP")
-        .push("M=D")
+      "// Start #{@string}
+      @#{index.to_i} // Put contant in A register
+      D=A            // Put contant in D register
+      @SP            // Put SP in A register
+      M=M+1          // Increment SP since we have it in A
+      A=M-1          // Put stack address in A register
+      M=D            // Write contant to stack
+      // End #{@string}"
       # Call instruction.new('whatever').to_assembly.each {|instruction| file.puts instruction }
     end
   end
