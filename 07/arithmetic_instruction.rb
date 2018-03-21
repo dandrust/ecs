182a70a00f4f9029ff116be7ef54c64a0806af7a
Dan Drust <dan.drust@protrainings.com>
2018-03-20 21:37:29 -0400

    Finish, refactor arithmetic instructions

--- 07/arithmetic_instruction.rb
+++ 07/arithmetic_instruction.rb
@@ -1,131 +1,100 @@
 class ArithmeticInstruction < Instruction
 
-
   def initialize operation
     @operation = operation
   end
 
   def to_assembly
     case @operation
-    # add, sub, and, or could be generalized...
+    when :add, :sub, :and, :or
+      binary_operation
+    when :neg, :not
+      unary_operation
+    when :eq, :gt, :lt
+      boolean_operation
+    end
+  end
+
+  private
+
+  def binary_operation
+    "// Start #{@string}
+    @SP    // Put SP in A register
+    AM=M-1 // Put address of 1st argument in A, decrement SP
+    D=M    // Put 1st argument in D
+    @SP
+    A=M-1  // Put address of 2nd argument in A
+    M=D#{operation_symbol}M  // Computer, write result to M
+    // End #{@string}"
+  end
+
+  def unary_operation
+    "// Start #{@string}
+    @SP    // Put SP in A register
+    A=M-1  // Put SP value in A and D registers
+    M=#{operation_symbol}M    // Computer, write result to M
+    // End #{@string}"
+  end
+
+  def boolean_operation
+    "// Start #{@string}
+    // Decrement SP
+    @SP
+    AM=M-1
+    // Store 1st argument
+    D=M
+    // Decrement SP
+    @SP
+    AM=M-1
+    // Compute difference, store in D register
+    D=M-D
+    / Conditional jump
+    @TRUE
+    D;#{jump_condition}
+
+    // False condition: Set D=0 for later storage
+    D=0
+    @END
+    0;JMP
+
+    // True condition: Set D=-1 for later storage
+    (TRUE)
+    D=-1 
+    @END
+    0;JMP
+
+    // Wrap Up: Write result and increment SP
+    (END)
+    @SP
+    M=M+1  // Increment SP since we have it in A
+    A=M-1  // Set A to what SP was before inc.
+    M=D
+    // End #{@string}"
+  end
+
+  def operation_symbol
+    case @operation
     when :add
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M-1")   # Load value of SP into A register
-        .push("D=M")   # load 1st val into data register
-        .push("A=A-1") # subtract one from stack pointer (2nd val)
-        .push("M=D+M") # write result of M+D to stack pointer
-        .push("D=A+1") # increment stack pointer
-        .push("@SP")
-        .push("M=D")
-    when :sub
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M-1")   # Load value of SP into A register
-        .push("D=M")   # load 1st val into data register
-        .push("A=A-1") # subtract one from stack pointer (2nd val)
-        .push("M=D-M") # write result of M+D to stack pointer
-        .push("D=A+1") # increment stack pointer
-        .push("@SP")
-        .push("M=D")
+      "+"
+    when :sub, :neg
+      "-"
     when :and
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M-1")  # The A, D registers has the address of the stack
-        .push("D=M") # put y in D reg
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("M=D&M") # write result of M+D to stack pointer
-        .push("D=A+1") # increment stack pointer
-        .push("@SP")
-        .push("M=D")
+      "&"
     when :or
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M-1")  # The A, D registers has the address of the stack
-        .push("D=M")
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("M=D|M") # write result of M+D to stack pointer
-        .push("D=A+1") # increment stack pointer
-        .push("@SP")
-        .push("M=D")
-    # neg and not could be generalized
-    when :neg
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("AD=M")  # The A, D registers has the address of the stack
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("M=-M")  # Negate value in memory, store in memory
-        .push("@SP")   # Load SP (0) into A register
-        .push("M=D")   # Set SP (0) to what it was (in D register)
+      "|"
     when :not
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("AD=M")  # The A, D registers has the address of the stack
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("M=!M")  # Negate value in memory, store in memory
-        .push("@SP")   # Load SP (0) into A register
-        .push("M=D")   # Set SP (0) to what it was (in D register)
-    # The remaining could be generalized
+      "!"
+    end
+  end
+  
+  def jump_condition
+    case @operation
     when :eq
-      # This, gt, and lt need some work with the jump business
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M-1")  # The A, D registers has the address of the stack
-        .push("D=M")
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("D=M-D") # Subtract x-y
-        .push("@TRUE") # THIS IS A PROBLEM -- were taking over A then referencing M later -- uh oh.  Need to store write address to SP before jump
-        .push("D;JEQ")
-        .push("D=0")
-        .push("M=D")
-        .push("@END")
-        .push("0;JMP")
-        .push("(TRUE)")
-        .push("D=-1") # this is FFFF -- all 1's -- true
-        .push("M=D")
-        .push("(END)")
-        .push("@SP")
-        .push("M=M+1")
-    when :gt
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M")  # The A, D registers has the address of the stack
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("D=M")
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("D=M-D") # Subtract x-y
-        .push("@TRUE")
-        .push("D;JGT")
-        .push("D=0")
-        .push("M=D")
-        .push("@END")
-        .push("0;JMP")
-        .push("(TRUE)")
-        .push("D=-1")
-        .push("M=D")
-        .push("(END)")
-        .push("@SP")
-        .push("M=M+1")
+      "JEQ"
     when :lt
-      Array.new
-        .push("@SP")   # Load address of stack pointer
-        .push("A=M")  # The A, D registers has the address of the stack
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("D=M")
-        .push("A=A-1") # subtract one from stack pointer (1st val)
-        .push("D=M-D") # Subtract x-y
-        .push("@TRUE")
-        .push("D;JLT")
-        .push("D=0")
-        .push("M=D")
-        .push("@END")
-        .push("0;JMP")
-        .push("(TRUE)")
-        .push("D=-1")
-        .push("M=D")
-        .push("(END)")
-        .push("@SP")
-        .push("M=M+1")
+      "JLT"
+    when :gt
+      "JGT"
     end
   end
 end
