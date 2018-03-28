302eca15b977a8efeadf63badbd1f56abdcc9d23
Dan Drust <dan.drust@protrainings.com>
2018-03-27 20:00:10 -0400

    Implement local, argument, this, and that segments for push operation

--- 07/push_instruction.rb
+++ 07/push_instruction.rb
@@ -1,23 +1,43 @@
 class Instruction::PushInstruction < Instruction
   
+  SEGMENT_SYMBOLS = {
+    :local => 'LCL',
+    :argument => 'ARG',
+    :this => 'THIS',
+    :that => 'THAT'
+  }
+
   def initialize *args
     @operation, @segment, @index = *args
   end
 
   def to_assembly
-    case @segment
+    <<-end_code
+    // Start #{operation} #{segment} #{index}
+    #{resolve_address}
+    @SP            // Put SP in A register
+    M=M+1          // Increment SP since we have it in A
+    A=M-1          // Put stack address in A register
+    M=D            // Write contant to stack
+    // End #{operation} #{segment} #{index}
+    end_code
+  end
+
+  def resolve_address
+    case segment
     when :constant
       <<-end_code
-      // Start #{@operation} #{@segment} #{@index}
       @#{index.to_i} // Put contant in A register
       D=A            // Put contant in D register
-      @SP            // Put SP in A register
-      M=M+1          // Increment SP since we have it in A
-      A=M-1          // Put stack address in A register
-      M=D            // Write contant to stack
-      // End #{@operation} #{@segment} #{@index}
       end_code
-      # Call instruction.new('whatever').to_assembly.each {|instruction| file.puts instruction }
+    else
+      <<-end_code
+      @#{SEGMENT_SYMBOL[segment]}
+      D=A            // Store base address in D
+      @#{index.to_i} // Put index in A register
+      D=D+A          // Add index to base address, store in D
+      end_code
     end
   end
+
 end
